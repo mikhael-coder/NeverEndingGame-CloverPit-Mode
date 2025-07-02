620522013831d6a67da60eca6cc891e1bece7d5a
Mikhael <135999238+mikhael-coder@users.noreply.github.com>
2025-07-02 14:08:51 +0300

    Add files via upload

---
+++ NeverEndingGame/Patchers.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+using System.Numerics;
+
+namespace CloverPitNeverEndingGame
+{
+    [HarmonyPatch]
+    public static class Patchers
+    {
+        [HarmonyPatch(typeof(GameplayData), "GetRewardBoxDebtIndex")]
+        [HarmonyPrefix]
+        static bool GetRewardBoxDebtIndex_Prefix(ref BigInteger __result)
+        {
+            if (Plugin.InfiniteMode.Value)
+            {
+                __result = 999999;
+            }
+            else
+            {
+                __result = Plugin.MaxDeadlines.Value - 1;
+            }
+            return false;
+        }
+    }
+}
---
+++ NeverEndingGame/Plugin.cs
@@ -0,0 +1,43 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using System.Linq;
+
+[BepInPlugin(Plugin.PLUGIN_GUID, Plugin.PLUGIN_NAME, Plugin.PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin
+{
+    public const string PLUGIN_GUID = "com.mikhaelo.cloverpitneverendinggame";
+    public const string PLUGIN_NAME = "CloverPit Never Ending Game";
+    public const string PLUGIN_VERSION = "1.0.0";
+
+    internal static ManualLogSource Log;
+
+    public static ConfigEntry<int> MaxDeadlines;
+    public static ConfigEntry<int> KeyCost;
+    public static ConfigEntry<bool> InfiniteMode;
+    public static ConfigEntry<float> DebtMultiplier;
+
+    private void Awake()
+    {
+        Log = Logger;
+        Logger.LogInfo($"Plugin {PLUGIN_GUID} v{PLUGIN_VERSION} loaded!");
+
+        MaxDeadlines = Config.Bind("General",
+            "MaxDeadlines",
+            5,
+            new ConfigDescription(
+                "Максимальное количество дедлайнов (0 = бесконечно)",
+                new AcceptableValueRange<int>(0, 100)));
+
+        InfiniteMode = Config.Bind("General",
+            "InfiniteMode",
+            false,
+            "Бесконечный режим игры");
+
+        var harmony = new Harmony(PLUGIN_GUID);
+        harmony.PatchAll();
+
+        Logger.LogInfo($"Applied {harmony.GetPatchedMethods().Count()} harmony patches");
+    }
+}
